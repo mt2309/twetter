d76b507dcced36b41ec790be3131846b9273ee43
Michael Thorpe <mt2309@ic.ac.uk>
2012-11-01 10:14:01 +0000

    Really initial commit to getting a split screen started

---
+++ lib/Twet/UI.pm
@@ -0,0 +1,10 @@
+package Twet::UI;
+
+use strict;
+use warnings;
+use utf8;
+# Need to output in utf8 since people have crazy characters in their names/tweets
+binmode STDOUT, ":utf8";
+
+use Term::Visual;
+
